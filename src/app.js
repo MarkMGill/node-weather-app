b62b38bc4ac9344a0e70a284774cfcdbe6bbe19a
MarkMGill <40337791+MarkMGill@users.noreply.github.com>
2020-05-17 09:35:46 -0400

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const hbs = require('hbs');
 const geocode = require('./utils/geocode');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -92,6 +93,6 @@ app.get('*', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('3000 yep');
+app.listen(port, () => {
+    console.log(`${port} yep`);
 });
